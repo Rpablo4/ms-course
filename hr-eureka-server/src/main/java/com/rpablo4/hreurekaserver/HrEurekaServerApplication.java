a8aaace0dae7911d287f10db4953bfba0d596197
Ruan Pablo <222ruanpablo@gmail.com>
2022-08-17 20:52:49 -0300

    Create Eureka Server

---
+++ hr-eureka-server/src/main/java/com/rpablo4/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.rpablo4.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
